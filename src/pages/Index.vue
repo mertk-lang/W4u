a68dd81742095d1e3619c214cd46b7fae3a77725
Mert <mertfor4_dev@outlook.com>
2021-05-18 23:41:31 +0300

    Pull data from API and display

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="flex column">
      <div class="col q-pa-lg">
-        <q-input v-model="search" dark borderless placeholder="Label">
+        <q-input v-model="search" @keyup.enter="getWeatherBySearch" dark borderless placeholder="Label">
           <template v-slot:before>
-            <q-icon name="my_location" />
+            <q-icon name="my_location" @click="getWeatherByLocation" />
           </template>
 
           <template v-slot:append>
-            <q-icon name="search" />
+            <q-icon @click="getWeatherBySearch" name="search" />
           </template>
           </q-input>
      </div>
@@ -15,18 +15,18 @@
      <template v-if="weatherData">
       <div class="col text-white text-center">
         <div class="text-h4 text-weight-light">
-          Tokyo
+          {{weatherData.name}}
         </div>
         <div class="text-h6 text-weight-light q-my-sm">
-          Rainy
+          {{weatherData.weather[0].description}}
         </div>
         <div class="text-h1 text-weight-thin q-my-lg relative-position">
-          4<span class="text-h3 degree relative-position ">&deg;</span>
+          {{Math.round(weatherData.main.temp)}}<span class="text-h3 degree relative-position ">&deg;</span>
         </div>
       </div>
 
       <div class="col text-center">
-        <img src="https://www.fillmurray.com/100/100" alt="">
+        <img :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`">
       </div>
      </template>
 
@@ -35,7 +35,7 @@
          <div class="col text-h2 text-weight-thin">
             What a lovely night
          </div>
-          <q-btn class="col location position-relative" size="1em" flat>
+          <q-btn @click="getWeatherByLocation" class="col location position-relative" size="1em" flat>
             <q-icon left size="2em" name="my_location" />
             <div>Find my location</div>
           </q-btn>
@@ -51,7 +51,37 @@ export default {
   data() {
     return {
       search: '',
-      weatherData: ''
+      weatherData: '',
+      errorMessage: '',
+      lat: '',
+      lon: '',
+      apiKey: 'db065681ac6cf240c4442a7ce130ce20'
+    }
+  },
+  methods: {
+    getWeatherByLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        this.getWeather()
+      }, (error) => {
+        this.errorMessage = error.message;
+        this.loadingState = false;
+      }, { timeout: 8000})
+    },
+    getWeather() {
+      let API = `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`;
+      this.$axios.get(API)
+      .then((res) => {
+        this.weatherData = res.data;
+      })
+    },
+    getWeatherBySearch() {
+       let API = `https://api.openweathermap.org/data/2.5/weather?q=${this.search}&appid=${this.apiKey}&units=metric`;
+      this.$axios.get(API)
+      .then((res) => {
+        this.weatherData = res.data;
+      })
     }
   }
 }
